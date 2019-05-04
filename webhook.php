8541aa47c829a06a67e7471276d24451e85c5797
HenrikAndWiktor <wiktor.istok.eriksson@gmail.com>
2019-05-04 20:46:50 +0200

    Fix web hook

--- webhook.php
+++ webhook.php
@@ -1,6 +1,2 @@
 <?php
-$entityBody = file_get_contents('php://input');
-$json_a = json_decode($entityBody, true);
-if (isset($json_a["hook"])) {
-    exec("cd /var/www/html;sudo git pull");
-}
+exec("cd /var/www/html;sudo git pull");
