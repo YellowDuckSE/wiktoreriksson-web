32adca6653c5d742e9b599072dd4434e9bc00cc2
HenrikAndWiktor <wiktor.istok.eriksson@gmail.com>
2020-03-12 09:36:40 +0100

    Update LAMP guide for RPi4

--- LAMP.php
+++ LAMP.php
@@ -54,13 +54,14 @@
             <thead><tr><th>Item</th><th>Purpose</th><th>Image</th></tr></thead>
             <tr><td>Raspberry Pi</td><td>Serves as the server itself</td><td><img src="img/rpi.jpg" alt="Raspberry Pi 3 Picture"></td></tr>
             <tr><td>SD or microSD (depending on which Raspberry Pi you have)</td><td>The storage solution</td><td><img src="img/sd.jpg" alt=""></td></tr>
-            <tr><td>Micro USB charger</td><td>Serves as the power source</td><td><img src="img/charger.jpg" alt="Basic Charger" style="height: 250px;width: 250px>"></td></tr>
-            <tr><td>HDMI (or Composite if the Raspberry Pi supports it) cable + screen</td><td>Display video</td><td><img src="img/hdmiscreen.jpg" alt="Monitor"><img src="img/hdmi.jpg" alt="HDMI Cable"></td></tr>
-            <tr><td>Network Cable</td><td>Networking. You can use the Wi-Fi of the Raspberry Pi 3 instead.</td><td><img src="img/ethernet.jpg" alt="Ethernet Cable"</td></tr>
+            <tr><td>Micro USB (or USB-C for RPi 4) charger</td><td>Serves as the power source</td><td><img src="img/charger.jpg" alt="Basic Charger" style="height: 250px;width: 250px>"></td></tr>
+            <tr><td>HDMI or micro-HDMI (or Composite if the Raspberry Pi supports it) cable + screen</td><td>Display video</td><td><img src="img/hdmiscreen.jpg" alt="Monitor"><img src="img/hdmi.jpg" alt="HDMI Cable"></td></tr>
+            <tr><td>Network Cable</td><td>Networking. You can use the Wi-Fi of the Raspberry Pi 3 and 4 instead.</td><td><img src="img/ethernet.jpg" alt="Ethernet Cable"</td></tr>
             <tfoot><tr><td><b>Item</b></td><td><b>Purpose</b></td><td><b>Image</b></td></tr></tfoot>
         </table>
     </div>
     <div class="dpart second">
+        <p class="browserupgrade">This section might be <strong>outdated</strong> as the Raspberry Pi 4 has now been released.</p>
         <h2>2. Install NOOBS on SD</h2>
         <p><em>Maybe you already installed Raspbian. If so, skip to <a href="#part4">Part 4.</a></em></p>
         <p>Now it's time to set up your Raspberry Pi. <a href="//downloads.raspberrypi.org/NOOBS_latest">Click here to get the latest NOOBS.</a></p>
@@ -69,6 +70,7 @@
         <p>When you´ve done that, insert the (micro)SD card in your Raspberry. Connect HDMI/Composite, network cable, and, at last, the charger.</p>
     </div>
     <div class="dpart first">
+        <p class="browserupgrade">This section might be <strong>outdated</strong> as the Raspberry Pi 4 has now been released.</p>
         <h2>3. Install "Raspbian"</h2>
         <p>A menu should show up. In the list, pick <b>Raspbian (recommended)</b> and follow instructions. I´ve been unable to test NOOBS, so my instructions can be a little bit unclear.</p>
         <p>When Raspbian boots, open Terminal and type:</p>
@@ -85,7 +87,7 @@
         <p>You're done there! <code>/var/www/html/</code> is the root of your web server. Add your web pages there!</p>
     </div>
     <div class="dpart first">
-        <h2>4. Port Forwarding</h2>
+        <h2>5. Port Forwarding</h2>
         <p>Follow <a href="//portforward.com/router.htm">this guide</a> to port forward. You will forward port <code>80</code>. Get your routers public IP address (for example mine is 78.108.50.45, DNS wiktoreriksson.se).</p>
         <p>Go to <code>http://YOUR_IP_ADDR/</code> in your browser. This will, hopefully, return your webpage! Now you can get a domain and register the IP to it!</p>
     </div>
